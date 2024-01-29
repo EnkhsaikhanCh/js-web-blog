6098411f32d87cf639d8e173894e615d58571ce9
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-01-29 21:09:01 +0800

    New components - Slider

---
+++ data/BlogData.jsx
@@ -0,0 +1,9 @@
+export const BlogData = [
+  {
+    id: 1,
+    title: "Grid system for better Design User Interface",
+    tag: "Technology",
+    uploadedDate: "August 20, 2022",
+    image: "/images/photo1.jpg",
+  },
+];
