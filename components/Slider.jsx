35be4bd3b949242aabe3eff900ab4129cfa0ca33
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-01-31 19:49:40 +0800

    Updated - Connected to BlogData

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -6,10 +6,9 @@ import Link from "next/link";
 export function Slider() {
   return (
     <div className="container mx-auto mt-[20px] flex flex-col gap-3 px-4 py-4">
-      <SliderCard />
-      {/* {BlogData.map((card, id) => (
-        <SliderCard key={id} {...card} />
-      ))} */}
+      {/* <SliderCard /> */}
+
+      <SliderCard blog={BlogData[0]} />
 
       <div className="hidden gap-1 self-end md:flex">
         <button className="rounded-md transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700">
@@ -28,25 +27,22 @@ function SliderCard({ blog }) {
 
   return (
     <Link
-      href="/link"
+      href={blog.link}
       key=""
       className="relative rounded-md border hover:dark:border-neutral-700"
     >
       <img
-        src="/images/photo1.jpg"
-        className="aspect-video h-[300px] w-full rounded-md object-cover md:h-[450px] lg:h-[600px]"
+        src={blog.image}
+        className="aspect-video h-[300px] w-full rounded-md border object-cover md:h-[450px] lg:h-[600px]"
       />
       <div className="w-100% group absolute bottom-0 left-0 right-0 flex flex-col gap-[24px] rounded-md bg-gradient-to-t from-slate-700 p-[40px] px-5 py-4 text-white transition-colors lg:bottom-2 lg:left-2 lg:right-2 lg:w-[600px] lg:border lg:border-transparent lg:bg-white lg:from-inherit lg:text-black">
         <div className="flex flex-col gap-[16px]">
           <p className="md:py-[4px]text-white flex w-[97px] items-center justify-center rounded-md bg-[#4B6BFB] text-white md:px-[10px]">
-            Design
+            {blog.tag}
           </p>
-          <h1 className="md:text-4xl">
-            The Impact of Technology on the Workplace: How Technology is
-            Changing
-          </h1>
+          <h1 className="md:text-4xl">{blog.title}</h1>
         </div>
-        <p className="hidden md:flex">August 20, 2022</p>
+        <p className="hidden md:flex">{blog.uploadedDate}</p>
       </div>
     </Link>
   );
