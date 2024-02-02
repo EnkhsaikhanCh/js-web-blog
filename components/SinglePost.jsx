2e07bdc4986328d907549a7304ecc7e54f67dc98
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-02-02 21:23:32 +0800

    Updated: Artical detail information data fetched from "Dev.to"

--- components/SinglePost.jsx
+++ components/SinglePost.jsx
@@ -1,39 +1,54 @@
 import { Header } from "./Header/Header";
 import { useEffect, useState } from "react";
+import parse from "html-react-parser";
+import { useRouter } from "next/router";
+import { Loading } from "./Loading";
 
-export function SinglePost() {
+export function SinglePost({ tags }) {
   const [article, setArticle] = useState();
+  const router = useRouter();
 
   useEffect(() => {
-    fetch("https://dev.to/api/articles/simonholdorf/[]")
-      .then((response) => response.json())
-      .then((data) => {
-        setArticle(data);
-      });
-  }, []);
+    if (router.query.username && router.query.slug) {
+      fetch(
+        `https://dev.to/api/articles/${router.query.username}/${router.query.slug}`,
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setArticle(data);
+        });
+    }
+  }, [router.query]);
 
-  if (article === undefined) return null;
+  console.log(router.query);
+
+  if (article === undefined) return <Loading />;
 
   return (
     <>
       <Header />
-      <div className="container mx-auto mt-[30px] flex w-[800px] flex-col gap-[32px]">
-        <div className="flex flex-col gap-5">
-          <h1 className="text-4xl font-bold	">{article.title}</h1>
-          <div className="flex gap-6 text-[#696A75]">
-            <img src="/profile" alt="" />
-            <p>{article.name}</p>
-            <p>{article.readable_publish_date}</p>
+      <div className="bg-[#f5f5f5] py-[30px]">
+        <div className="container mx-auto flex w-[800px] flex-col gap-[15px] rounded-md bg-white pb-4">
+          <img
+            src={article.cover_image}
+            alt=""
+            className="aspect-video h-[400px] w-[800px] rounded-md object-cover"
+          />
+          <div className="mx-3 flex flex-col gap-5">
+            <div className="flex items-center justify-between gap-6 rounded-md bg-gray-100 px-5 py-2 text-[#696A75]">
+              <div className="flex items-center justify-center gap-4">
+                <img
+                  src={article.user.profile_image}
+                  alt=""
+                  className="h-[35px] w-[35px] rounded-full"
+                />
+                <p>{article.user.username}</p>
+              </div>
+              <p>{article.readable_publish_date}</p>
+            </div>
+            <h1 className="text-center text-4xl	font-bold">{article.title}</h1>
           </div>
-        </div>
-
-        <img
-          src={article.cover_image}
-          alt=""
-          className="aspect-video h-[462px] w-[800px] rounded-md object-cover"
-        />
-        <div>
-          <p>{article.description}</p>
+          <div className="prose mx-auto">{parse(article.body_html)}</div>
         </div>
       </div>
     </>
