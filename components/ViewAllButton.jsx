21bc0eeb3289aaae4391f7fdccece90d0e1b5bb0
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-02-06 20:27:38 +0800

    New component - ViewAllButton

---
+++ components/ViewAllButton.jsx
@@ -0,0 +1,7 @@
+export function ViewAllButton() {
+  return (
+    <button className="rounded-md border px-4 py-1 hover:border-slate-300 hover:bg-gray-50">
+      <a href="/blog">View all</a>
+    </button>
+  );
+}
