8805321f797014337c1565a0b81ddd1eb98dee07
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-02-07 13:55:11 +0800

    minor improvements

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <div className="mx-auto px-4 py-4 md:container md:px-0">
-      <div className="flex items-center justify-between">
+    <div className="mx-auto px-4 py-4 md:container md:px-0 sticky top-0 z-10 bg-[hsla(0,0%,100%,.9)]">
+      <div className="flex items-center justify-between px-4">
         <Link href="http://localhost:3000/">
           <Logo />
         </Link>
