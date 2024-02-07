bfafab5fb35966b1943979a01ccd9ea33ee49ba0
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-02-07 18:14:56 +0800

    Fixed - LoadNext

--- components/LoadNext.jsx
+++ components/LoadNext.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
 
+export function LoadNext({ articles, setArticles }) {
+  const [currentPage, setCurrentPage] = useState(1);
 
-export function LoadNext({ loadNext }) {
+  const username = "simonholdorf";
+  const apiUrl = `https://dev.to/api/articles?username=${username}&per_page=`;
+  const itemsPerPage = 9;
+
+  function loadNext() {
+    fetch(`${apiUrl}${itemsPerPage}&page=${currentPage + 1}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setArticles([...articles, ...data]);
+        setCurrentPage(currentPage + 1);
+      });
+  }
+
   return (
     <div className="mb-[70px] flex items-center justify-center">
       <button
