32d665c4c54d929ee6703d500ea8ee158be1af5a
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-03-02 23:50:00 +0800

    Updated - ThemeChanger

--- components/Header/ThemeChanger.jsx
+++ components/Header/ThemeChanger.jsx
@@ -1,35 +1,29 @@
 import { useEffect, useState } from "react";
-import { CiSun } from "react-icons/ci";
-import { LuMoon } from "react-icons/lu";
+import { IoIosSunny as CiSun, IoIosMoon as LuMoon } from "react-icons/io"; // Corrected imports
 
 export function ThemeChanger() {
-  const [theme, setTheme] = useState("light");
-
-  function changeTheme() {
-    if (theme === "light") {
-      setTheme("dark");
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      setTheme("light");
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    }
-  }
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
 
   useEffect(() => {
-    const localTheme = localStorage.getItem("theme");
-    setTheme(localTheme);
-    document.documentElement.classList.add(localTheme);
-  }, []);
+    document.documentElement.classList.add(theme);
+    return () => {
+      document.documentElement.classList.remove(theme);
+    };
+  }, [theme]);
+
+  function changeTheme() {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    document.documentElement.classList.replace(theme, newTheme);
+    localStorage.setItem("theme", newTheme);
+  }
 
   return (
     <button
       className="flex h-[36px] w-[36px] items-center justify-center rounded-md border border-[#F4F4F5] bg-[#F4F4F5] hover:border-blue-300 dark:border-[#39404f] dark:bg-[#39404f] dark:hover:border-blue-300"
       onClick={changeTheme}
     >
-      {theme === "light" && <CiSun />}
-      {theme === "dark" && <LuMoon className="dark:text-white" />}
+      {theme === "light" ? <CiSun /> : <LuMoon className="text-white" />}
     </button>
   );
 }
