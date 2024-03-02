9d807bab535c337e6c818607776ba0d4354459a8
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-03-03 00:02:29 +0800

    Updated - Trending

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,18 +1,24 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { Loading } from "./Loading";
 
 export function Trending() {
   const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const username = "simonholdorf";
   const apiUrl = `https://dev.to/api/articles?username=${username}&top=30`;
-  const itemsPerPage = 4;
 
   useEffect(() => {
-    fetch(`${apiUrl}&per_page=${itemsPerPage}&page=1&tag=devops`)
+    fetch(`${apiUrl}&per_page=4&page=1&tag=devops`)
       .then((response) => response.json())
       .then((data) => {
-        setArticles([...articles, ...data]);
+        setArticles(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Fetching error:", error);
+        setIsLoading(false);
       });
   }, []);
 
@@ -24,27 +30,34 @@ export function Trending() {
         DevOps
       </h1>
       <div className="grid gap-3 md:grid-cols-2 lg:grid-cols-4">
-        {articles.map((card, id) => (
-          <TrendingCard key={id} {...card} />
+        {articles.map((article) => (
+          <TrendingCard key={article.id} {...article} />
         ))}
       </div>
     </div>
   );
 }
 
-const TrendingCard = (props) => {
-  const { id, path, social_image, tag_list, title } = props;
+const TrendingCard = ({ id, path, social_image, tag_list, title }) => {
+  // const { id, path, social_image, tag_list, title } = props;
   return (
     <Link
       key={id}
       href={path}
       className="relative flex flex-col rounded-md border hover:border-blue-300 dark:border-[#2A303C] dark:hover:border-blue-300"
     >
-      <img src={social_image} alt={title} className="rounded-md" />
+      <img
+        src={social_image}
+        alt={`Cover image for ${title}`}
+        className="rounded-md"
+      />
       <div className="absolute bottom-0 flex w-full flex-col gap-2 rounded-md bg-gradient-to-t from-slate-700 p-4 text-white">
         <div className="mb-2 hidden gap-2">
-          {tag_list.map((tag) => (
-            <span className="inline-block rounded bg-blue-200 px-[10px] py-1 text-sm text-[#4B6BFB]">
+          {tag_list.map((tag, index) => (
+            <span
+              key={index}
+              className="inline-block rounded bg-blue-200 px-[10px] py-1 text-sm text-[#4B6BFB]"
+            >
               {tag}
             </span>
           ))}
