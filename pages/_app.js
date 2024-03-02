18b5db4edda2445a66bffeb56ba280686f31120f
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-03-02 21:40:25 +0800

    Installed - Vercel's speed-insights & analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} /> ;
+      <SpeedInsights />
+      <Analytics />
+    </>
+  );
 }
