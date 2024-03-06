5f3a9af96d737626e5d0efe52bf364005099d9d9
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-03-07 00:13:20 +0800

    minor improvements

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import parse from "html-react-parser";
 import hljs from "highlight.js";
 import "highlight.js/styles/atom-one-dark.css";
@@ -41,48 +42,60 @@ export default function Home({ article = {} }) {
         <div>
           <div className="container mx-auto flex w-full flex-col gap-4 rounded-md  pb-4 dark:text-[#ADBAC7] lg:w-[900px]">
             <ArticleBackground_Img article={article} />
-            <div className="flex w-[100%] flex-col gap-4">
-              <User_Info article={article} />
-              <ArticleContent parse={parse} article={article} />
-            </div>
+            <User_Info article={article} />
+            <ArticleContent parse={parse} article={article} />
           </div>
         </div>
-        <Footer />
       </div>
+      <Footer />
     </>
   );
 }
 
 function ArticleBackground_Img({ article }) {
-  if (!article || !article.social_image) {
-    return <div>No image available</div>;
-  }
-
   return (
     <div className="px-4 pt-[30px]">
-      <img
-        src={article.social_image}
-        alt={`Image for ${article.title}`}
-        className="aspect-video h-[200px] w-full rounded-md object-cover md:h-[300px] lg:h-[400px] lg:w-[900px]"
-      />
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          paddingTop: "56.25%",
+          height: 0,
+          overflow: "hidden",
+          borderRadius: "8px",
+        }}
+      >
+        <Image
+          src={article.social_image}
+          alt={`Image for ${article.title}`}
+          layout="fill"
+          objectFit="cover"
+          priority
+        />
+      </div>
     </div>
   );
 }
 
 function User_Info({ article }) {
-  if (!article || !article.user) {
-    return <div>User information is not available</div>;
-  }
-
   return (
     <div className="px-4">
       <div className="flex items-center justify-between gap-6 rounded-md border bg-white px-5 py-2 text-[#696A75] dark:border-[#2D333B] dark:bg-[#2D333B] dark:text-[#ADBAC7]">
         <div className="flex items-center justify-center gap-4 ">
-          <img
-            src={article.user.profile_image}
-            alt={article.user.username}
-            className="h-[35px] w-[35px] rounded-full"
-          />
+          <div
+            className="overflow-hidden rounded-full"
+            style={{ width: 35, height: 35 }}
+          >
+            <Image
+              src={article.user.profile_image}
+              alt={article.user.username}
+              width={35}
+              height={35}
+              layout="fixed" // This layout mode respects the given width and height
+              placeholder="blur"
+              blurDataURL="data:image/jpeg;base64,..."
+            />
+          </div>
           <p className="text-blue-500 dark:text-blue-300">
             {article.user.username}
           </p>
@@ -94,10 +107,6 @@ function User_Info({ article }) {
 }
 
 function ArticleContent({ parse, article }) {
-  if (!article || !article.title || !article.body_html) {
-    return <div>Article content is not available.</div>;
-  }
-
   return (
     <div className="pb-[30px] md:px-4">
       <div className="rounded-md border bg-white px-1 py-4 dark:border-[#2D333B] dark:bg-[#2D333B]">
